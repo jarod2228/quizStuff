fbbcb98fe021ce0cdd98050582b83ed17508a2e3
Jarod Waggener <jarod2228@rocketmail.com>
2021-05-12 16:42:26 -0500

    finished looking over studio, will need to do it again from scratch

--- src/quizStuff/Checkbox.java
+++ src/quizStuff/Checkbox.java
@@ -1,5 +1,53 @@
 package quizStuff;
 
-public class Checkbox {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Checkbox extends Question{
+
+    private ArrayList<String> possibleAnswers;
+    private ArrayList<Integer> correctAnswers;
+
+    public Checkbox(int pointValue, String text, ArrayList<String> possibleAnswers, ArrayList<Integer> correctAnswers) {
+        super(pointValue, text);
+        this.possibleAnswers = possibleAnswers;
+        this.correctAnswers = correctAnswers;
+        setPointValue(this.correctAnswers.size());
+    }
+
+    public int checkAnswers(ArrayList<Integer> check) {
+        int answersCorrect = 0;
+        for (int i = 0; i < check.size(); i++) {
+            if (correctAnswers.contains(check.get(i))) {
+                answersCorrect ++;
+            }
+        }
+        return answersCorrect;
+    }
+
+    @Override
+    public void displayAnswers() {
+        for(int i = 0; i < possibleAnswers.size(); i++) {
+            System.out.println(possibleAnswers.get(i));
+        }
+    }
+    @Override
+    public int getAnswers() {
+        ArrayList<Integer> usersAnswers = new ArrayList<>();
+        System.out.println("For this question, there is more than one correct answer. Enter 'Y' to begin answering the question.");
+        Scanner userContinue = new Scanner(System.in);
+        String goingToContinue = userContinue.nextLine();
+        while (goingToContinue.indexOf('y') >= 0 && usersAnswers.size() <= possibleAnswers.size()) {
+            System.out.println("What will your answers be? Enter the number of the option you believe is correct. For example, to select the first option, enter option 1");
+            Scanner userAnswer = new Scanner(System.in);
+            String answer = userAnswer.nextLine();
+            int option = Integer.parseInt(answer);
+            usersAnswers.add(option);
+
+            System.out.println("For this question, there are multiple correct answers. To continue answering the question, enter 'y'.");
+            goingToContinue = userContinue.nextLine();
+        }
+        return checkAnswers(usersAnswers);
+
+    }
 }
